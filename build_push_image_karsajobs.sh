cc187a8743a17628baa221141fe365b1795fdea4
Ubuntu <ubuntu@ip-172-31-0-33.ap-southeast-3.compute.internal>
2024-08-06 18:08:42 +0000

    push karsajob

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,10 +1,8 @@
 # build image dari dockerfile pada directory itu
-docker build -t item-app:v1 /home/ubuntu/a433-microservices/
+docker build -t ghcr.io/"$DOCKER_USERNAME"/karsajobs:latest .
 # login ke github package dgn user github dan pw token
 echo "$DOCKER_PASSWORD" | docker login ghcr.io -u "$DOCKER_USERNAME" --password-stdin
-# rubah nama image sesuai online repo
-docker tag item-app:v1 ghcr.io/"$DOCKER_USERNAME"/item-app:v1
 # push ke online repo
-docker push ghcr.io/"$DOCKER_USERNAME"/item-app:v1
+docker push ghcr.io/"$DOCKER_USERNAME"/karsajobs:latest
 # logout github package
 docker logout ghcr.io
